7c238cf6349c531ee57cfd93b1daca5850f3cee6
Ryan Svihla <rs@foundev.pro>
2020-07-15 16:17:05 +0000

    stopping point

--- pkg/cmd/sysbottle.go
+++ pkg/cmd/sysbottle.go
@@ -54,8 +54,8 @@ func ExecSysBottle(args []string) {
 
 //RunSysBottle parses the iostat file and generates a report based on the command line flags
 func RunSysBottle(cpu int, diskQueue float64, disks string, ioWait int, threshold int, file string) (string, error) {
-	var report string
-	return report, nil
+	reporter := NewSysBottleReport(file, &SysBottleConf{})
+	return "", reporter.PrintReport()
 }
 
 type CPUMeasurement struct {
@@ -96,10 +96,13 @@ var DEVICE = "device"
 
 //results := make(chan IOStatRow)
 //us, eu date formats, and one seen in sper66
+var datefmts []string = []string{
+	"01/02/2006 03:04:05 PM",
+	"02/01/2006 15:04:05",
+	"01/02/2006 15:04:05",
+}
 
-var datefmts []string = []string{"%m/%d/%Y %I:%M:%S %p", "%d/%m/%y %H:%M:%S", "%m/%d/%y %H:%M:%S"}
-
-func (s *IOStatParser) ParseRow(results chan IOStatRow, line string) error {
+func (s *IOStatParser) parseRow(results chan IOStatRow, line string) error {
 	if line == "\n" { // empty lines are the reset switch
 		if s.state == DEVICE {
 			results <- s.iostat
@@ -187,19 +190,240 @@ func (s *IOStatParser) parseDate(line string) error {
 	return nil
 }
 
-func (s *IOStatParser) Parse(infile string) (chan IOStatRow, error) {
+func (s *IOStatParser) parseFile(infile string) (chan IOStatRow, chan<- error) {
 	//parse an iostat file
 	results := make(chan IOStatRow)
+	errCh := make(chan error)
 	f, err := os.Open(infile)
 	if err != nil {
-		return results, fmt.Errorf("unable to open file %s with error %s", infile, err)
+		errCh <- fmt.Errorf("unable to open file %s with error %s", infile, err)
+		return results, errCh
 	}
 	defer f.Close()
-	scanner := bufio.NewScanner(f)
-	for scanner.Scan() {
-		if err := s.ParseRow(results, scanner.Text()); err != nil {
-			return results, fmt.Errorf("unable to parse row with error %s", err)
+	go func() {
+		scanner := bufio.NewScanner(f)
+		for scanner.Scan() {
+			if err := s.parseRow(results, scanner.Text()); err != nil {
+				errCh <- fmt.Errorf("unable to parse row with error %s", err)
+				return
+			}
+		}
+	}()
+	return results, errCh
+}
+
+type SysBottleConf struct {
+	IOWaitThreshold int
+	CPUThreshold    int
+	Disks           []string
+	QueueThreshold  int
+	BusyThreshold   int
+}
+
+//Produces a report from iostat output"
+type SysBottleReport struct {
+	infile         string
+	parser         IOStatParser
+	count          int
+	cpuExceeded    int
+	ioWaitExceeded int
+	cpuStats       map[string][]float64
+	queueDepth     map[string]int
+	devices        map[string]func() map[string][]string
+	start          time.Time
+	end            time.Time
+	deviceIndex    map[string]int
+	cpuIndex       map[string]int
+	conf           SysBottleConf
+	recs           map[string]bool
+	analyzed       bool
+}
+
+func NewSysBottleReport(infile string, conf *SysBottleConf) *SysBottleReport {
+	s := SysBottleReport{}
+	s.infile = infile
+	s.parser = IOStatParser{}
+
+	s.count = 0
+	s.cpuExceeded = 0
+	s.ioWaitExceeded = 0
+	s.devices = make(map[string]func() map[string][]string)
+	s.cpuStats = make(map[string][]float64)
+	s.queueDepth = make(map[string]int)
+	s.deviceIndex = make(map[string]int)
+	s.cpuIndex = make(map[string]int)
+	if conf == nil {
+		s.mkConf()
+	} else {
+		s.conf = *conf
+	}
+	s.recs = make(map[string]bool)
+	s.analyzed = false
+	return &s
+}
+
+func (s *SysBottleReport) mkConf() SysBottleConf {
+	return SysBottleConf{
+		IOWaitThreshold: 5,
+		CPUThreshold:    50,
+		Disks:           []string{},
+		QueueThreshold:  1,
+		BusyThreshold:   5,
+	}
+}
+
+func (s *SysBottleReport) analyze() error {
+	//analyzes the file this class was initialized with
+	ch, err := s.parser.parseFile(s.infile)
+	if err != nil {
+		return fmt.Errorf("unable to analyze the file with error: %s", err)
+	}
+
+	for row := range ch {
+		s.count += 1
+		if s.deviceIndex != nil {
+			s.mkColIDX(row)
+		}
+		s.analyzeDisk(row)
+		s.analyzeCPU(row)
+		if !s.start.IsZero() {
+			s.start = row.Date
+		}
+		if !s.end.IsZero() || row.Date.After(s.end) {
+			s.end = row.Date
+		}
+		s.analyzed = true
+	}
+	return nil
+}
+
+func (s *SysBottleReport) mkColIDX(stat IOStatRow) {
+	for i, col := range stat.Device.Cols {
+		s.deviceIndex[col] = i
+	}
+	for i, col := range stat.CPU.Cols {
+		s.cpuIndex[col] = i
+	}
+}
+
+func (s *SysBottleReport) wantDisk(name string) bool {
+	if len(s.conf.Disks) == 0 {
+		//this is not super obvious but the "no disks" case means all disks match
+		return true
+	}
+	for _, diskName := range s.conf.Disks {
+		if diskName == name {
+			return true
 		}
 	}
-	return results, nil
+	return false
+}
+
+func (s *SysBottleReport) analyzeDisk(stat IOStatRow) {
+	for disk, values := range stat.Device.Stat {
+		if s.wantDisk(disk) {
+			for col := range s.deviceIndex {
+				val := values[s.deviceIndex[col]]
+				s.devices[disk][col] = append(s.devices[disk][col], val)
+				//TODO fix this int casting to val this was a bug in the old python code that was comparing ints and floats
+				if strings.Contains(col, "qu") && int(val) >= s.conf.QueueThreshold {
+					s.queueDepth[disk] += 1
+					s.recs[fmt.Sprintf("* decrease activity on %s", disk)] = true
+				}
+			}
+		}
+	}
+}
+
+func (s *SysBottleReport) analyzeCPU(stat IOStatRow) {
+	total := 0
+	for _, cpu := range []string{"system", "user", "nice", "steal"} {
+		//TODO fix this casting, this was a bug in the old python codebase
+		//that never got caught
+		total += int(stat.CPU.Stat[s.cpuIndex["%"+cpu]])
+	}
+	//TODO fix this casting, this was a bug in the old python codebase
+	//that never got caught
+	s.cpuStats["total"] = append(s.cpuStats["total"], float64(total))
+	if total > s.conf.CPUThreshold {
+		s.cpuExceeded += 1
+		s.recs["* tune for less CPU usage"] = true
+	}
+	for col := range s.cpuIndex {
+		val := stat.CPU.Stat[s.cpuIndex[col]]
+		s.cpuStats[col] = append(s.cpuStats[col], val)
+	}
+	ioWaitIndex := s.cpuIndex["%iowait"]
+	//TODO fix this casting, this was a bug in the old python codebase
+	//that never got caught
+	if int(stat.CPU.Stat[ioWaitIndex]) > s.conf.IOWaitThreshold {
+		s.ioWaitExceeded += 1
+		s.recs["* tune for less IO"] = true
+	}
+}
+
+//PrintReport prints a report for the file this class was initialized with, analyzing if necessary"
+func (s *SysBottleReport) PrintReport() error {
+	if !s.analyzed {
+		if err := s.analyze(); err != nil {
+			return fmt.Errorf("unable to generate analysis %s", err)
+		}
+	}
+	fmt.Println("sysbottle\n")
+	fmt.Printf("* total records: %s\n", s.count)
+	if s.count > 0 {
+		reportPercentage :=
+			func(a int) float64 {
+				return (float64(a) / float64(s.count)) * 100.0
+			}
+		fmt.Printf("* total bottleneck time: %.2f%% (cpu bound, io bound, or both)\n",
+			reportPercentage(s.ioWaitExceeded+s.cpuExceeded))
+		fmt.Printf("* cpu+system+nice+steal time > %.2f%%: %.2f%%\n",
+			s.conf.CPUThreshold, reportPercentage(s.cpuExceeded))
+		fmt.Printf("* iowait time > %.2f%%: %.2f%%\n",
+			s.conf.IOWaitThreshold, reportPercentage(s.ioWaitExceeded))
+		fmt.Printf("* start %s\n", s.start)
+		fmt.Printf("* end %s\n", s.end)
+		logTimeSeconds := s.end.Second() - s.start.Second() + 1
+		fmt.Printf("* log time: %ss\n", logTimeSeconds)
+		fmt.Printf("* interval: %ss\n", reportPercentage(logTimeSeconds))
+		for device := range s.devices {
+			fmt.Printf("* %s time at queue depth >= %.2f: %.2f%%\n",
+				device, s.conf.QueueThreshold, reportPercentage(s.queueDepth[device]))
+		}
+		fmt.Println("")
+		var lines [][]string
+		lines = append(lines, getPercentileHeaders())
+		lines = append(lines, []string{"", "---", "---", "---", "---", "---", "---"})
+		lines = append(lines, getPercentiles("cpu", s.cpuStats["total"]))
+		lines = append(lines, getPercentiles("iowait", s.cpuStats["%iowait"]))
+		lines = append(lines, []string{})
+		lines = append(lines, getPercentileHeaders())
+		lines = append(lines, []string{"", "---", "---", "---", "---", "---", "---"})
+		for device := range s.devices {
+			lines = append(lines, []string{device, "", "", "", "", "", ""})
+			for iotype := range s.devices[device]() {
+				if strings.Contains(iotype, "qu") || strings.Contains(iotype, "wait") {
+					lines = append(lines, getPercentiles("- "+iotype+":", s.devices[device][iotype]))
+				}
+			}
+		}
+		lines = append(lines, []string{})
+		humanize.padTable(lines, 8, 2)
+		for _, line := range lines {
+			fmt.Println(strings.Join(line, ""))
+		}
+		s.printRecommendations()
+	}
+}
+
+func (s *SysBottleReport) printRecommendations() {
+	if len(s.recs) == 0 {
+		return
+	}
+	fmt.Println("recommendations")
+	fmt.Println(strings.Repeat("-", 15))
+	for _, rec := range s.recs {
+		fmt.Println(rec)
+	}
 }
